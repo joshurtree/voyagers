af34dcaed03bed3abc1bea78c74dac9946203243
joshurtree <joshurtree@gmail.com>
2022-08-19 22:21:33 +0100

    remove cypress from repository

--- cypress/integration/interval-picker-spec.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { mount } from '@cypress/react';
-import { IntervalPicker } from './components/interval-picker'
-
-describe('Test IntervalPicker component', () => {
-  mount(<IntervalPicker onChange={() => true}/>);
-});
--- cypress/integration/voyage-filter-spec.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { mount } from '@cypress/react';
-import { VoyageFilter } from './components/voyage-filter';
-
-describe('Render VoyageFilter', () => {
-  mount(<VoyageFilter onChange={() => true} players={[]} />);
-});
